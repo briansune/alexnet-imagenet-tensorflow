1114aea41450a6870556f0ce2bd4aa6950f618d8
Briansune <briansune@gmail.com>
2019-01-07 19:25:58 +0800

    Update alexnet_test.sh

--- alexnet_test.sh
+++ alexnet_test.sh
@@ -2,10 +2,10 @@
 set -e
 
 
-INFO=/H/Tensorflow_Log/tensor_info
-LOGS=/H/Tensorflow_Log/log
+INFO=######/Tensorflow_Log/tensor_info
+LOGS=######/Tensorflow_Log/log
 
 GLOG_logtostderr=0 \
 GLOG_log_dir=$INFO \
-python -u test.py > $LOGS/alexnet_elu_test.log 2>&1 \
+python -u ../test.py > $LOGS/alexnet_test_######.log 2>&1 \
 $@
